fc40bf158181fafc08b93da43001e55e0991e097
Unay Santisteban <davidu@softcom.com>
2017-08-08 13:23:54 +0200

    Added feature for hide menu on scroll.

--- js/othercode.js
+++ js/othercode.js
@@ -8,5 +8,14 @@ jQuery(document).ready(function () {
         if (!url.test(anchors[i].href)) continue;
         anchors[i].setAttribute('data-scroll', true);
     }
-    
+
+    if (jQuery('#g-navigation').css('position') === 'fixed'){
+        jQuery(window).scroll(function () {
+            if (jQuery(this).scrollTop() > 200) {
+                jQuery('#g-navigation').fadeIn(500);
+            } else {
+                jQuery('#g-navigation').fadeOut(500);
+            }
+        });
+    }
 });
