472efc6d87ee4997f029f293bf0f5dad1d6f6804
Unay Santisteban <davidu@softcom.com>
2017-08-09 09:14:30 +0200

    Js files updated. Still having some issue with the Smooth Scroll feature...

--- js/othercode.js
+++ js/othercode.js
@@ -1,9 +1,18 @@
 jQuery(document).ready(function () {
 
+    var anchors = document.querySelectorAll('a[href*="#"]');
+    for (var i = 0, len = anchors.length; i < len; i++) {
+        var url = new RegExp(window.location.hostname + window.location.pathname);
+        if (!url.test(anchors[i].href)) {
+            continue;
+        }
+        anchors[i].setAttribute('data-scroll', '');
+    }
+
     var wow = new WOW().init();
     var smooth = new SmoothScroll('a[href*="#"]');
 
-    if (jQuery('#g-navigation').css('position') === 'fixed'){
+    if (jQuery('#g-navigation').css('position') === 'fixed') {
         jQuery(window).scroll(function () {
             if (jQuery(this).scrollTop() > 200) {
                 jQuery('#g-navigation').fadeIn(500);
